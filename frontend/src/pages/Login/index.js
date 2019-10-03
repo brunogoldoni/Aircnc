e11a818f285662faedecd4e4a0a3d8889953058e
Bruno Mendonça Goldoni <bgoldoni@orion.tech>
2019-10-03 16:31:21 -0300

    Interface Web
    -Builder interface web

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+export default function Login() {
+
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
+  return (
+    <>
+      <p>
+        Ofereça <strong>spots</strong> para progamadores e encontre <strong>talentos</strong> para sua empresa
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-mail *</label>
+        <input
+          id="email"
+          placeholder="Seu melhor e-mail"
+          type="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+
+        <button className="btn" type="submit">Entrar</button>
+      </form>
+    </>
+  )
+}
