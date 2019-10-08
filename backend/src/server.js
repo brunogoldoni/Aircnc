2c47a07326c13e3fd4d6ded3e5a7c98da2502375
Bruno Mendonça Goldoni <bgoldoni@orion.tech>
2019-10-08 17:23:00 -0300

    Advanced Features
    -socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,18 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on('conection', socket => {
+  console.log('Usuário conectado', socket.id);
+});
 
 mongoose.connect('mongodb+srv://bruno:brunodb@heineken-mlhas.mongodb.net/semana09?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -22,4 +31,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 // use (routes) should always come after express.json because express reads sequentially
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import api from '../../services/api';
 
 import './styles.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    const socket = socketio('http://localhost:3333');
+  }, []);
+
   useEffect(() => {
     async function loadSpots() {
       const user_id = localStorage.getItem('user');
